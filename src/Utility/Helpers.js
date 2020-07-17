685d87902a768e742e8f86ff8acd236c5773ac66
Seth <sethbalodi@gmail.com>
2020-07-16 23:55:56 -0400

    caught up

--- src/Utility/Helpers.js
+++ src/Utility/Helpers.js
@@ -8,7 +8,9 @@
   * @param { if we only want tags from a particular category} currentCategory 
   */
  export function getAllTagsForSection(data=[], currentCategory=false) {
+   console.log('function getAlLTagsForSection :: data', data, 'currentCategory', currentCategory);
     const category = (currentCategory) ? currentCategory : null;
+    console.log('cateegory?', category);
     const tagsForAllCategories = data.map((element) => {
         const categories = element.node.frontmatter.categories;
         const tags = element.node.frontmatter.tags;
@@ -17,11 +19,14 @@
         }
       });
 
+      console.log('>> what is tagsForAllCategories', tagsForAllCategories);
+
       //flatten all tags
       const flattened_tagsForAllCategories = [].concat.apply([], tagsForAllCategories);
 
       //remove all nulls
       const filtered = flattened_tagsForAllCategories.filter(Boolean);
+      console.log('what is filtered?', filtered);
 
       // return a new unique set
       return Array.from(new Set(filtered));
