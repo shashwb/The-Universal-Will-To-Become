78ba4129a287dfe81daf004465553d647a2be4da
Seth <sethbalodi@gmail.com>
2020-08-01 22:57:34 -0400

    added an About section

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+
 
 
 /**
@@ -8,9 +10,74 @@ import React from 'react'
  */
 const About = () => (
   <>
-    <h1>Insert Kurt Vonnegut Quote</h1>
-    <h2>Explain the different "Shapes"</h2>
-    <h3>Who am I? Developer based in New York City</h3>
+    <h1>What Is All This Here About?</h1>
+
+    <p>
+     This blog is a loose collection of essays, videos and podcasts with no planned coherence.
+     The eventual cosmic fate of these ideas is to become contributions
+     to the <b>Chrono-Synclastic Infundibulae</b>,
+     wherever they may exist throughout our galaxy.
+    </p>
+
+    <p>Some explanation of terms:</p>
+
+    <p>
+      From novelist and scruffy hair enthusiast Kurt Vonnegut's <i>Sirens of Titan</i>, a <b>Chrono-Synclastic Infundibulum</b> is described as the following:
+    </p>
+
+    <blockquote>
+      <p>
+        <i>
+        "<b><i>Chrono</i></b> means time. 
+         <b><i> Synclastic</i></b> means curved towards the same side in all directions, like the skin of an orange. 
+         <b><i> Infundibulum</i></b> is what ancient Romans like Julius Caesar and Nero called a funnel..."
+        </i>
+      </p>
+    </blockquote>
+
+    <p>
+     Vonnegut describes places in our Universe where all of the myriad lowercase "t" and capital "T" truths co-exist in contradictory harmony to each other, colliding into metaphorical helixes, like philosophical DNA strands. Vonnegut again:
+    </p>
+
+    <blockquote>
+      <p>
+        <i>
+          "These places are where all the different kinds of truths fit togther as nicely as the parts in your ... solar watch."
+        </i>
+      </p>
+    </blockquote>
+
+    <p>
+      There are nooks and crannies within our own minds where all kinds of truths fit together. We are exceptional at keeping a harmonious cognitive dissonance, which I theorize is part of our strategy to scoff at an uncaring universe, to sneer at the cosmic last laugh.
+      
+      I like the idea that there is a physical place (or places!) where the Universe has decided to do a little consideration of it's own, like a giant cosmic <a href="https://en.wikipedia.org/wiki/The_Thinker">The Thinker</a> sculpture.
+      All good fiction is based on wishful thinking! These <b>Shapes of Stories</b>, as well, represent an honest attempt, goddammit, at wishful thinking.
+    </p>
+
+    <div style={{ margin: 'auto', maxWidth: '300px', width: '100%' }}>
+      <Paper>
+        <img style={{ marginBottom: '0px' }} src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Le_Penseur_in_the_Jardin_du_Mus%C3%A9e_Rodin%2C_Paris_14_June_2015.jpg"></img>
+        <figcaption style={{ marginBottom: '0px' }} >
+          <p style={{ marginBottom: '0px', color: '#928f8f' }}>Fig.1 - He's probably thinking of something wishful.</p>
+        </figcaption>
+      </Paper>
+    </div>
+    
+
+    
+    
+    <p>
+    The goal is to have a good heuristic for the <a href="https://en.wikipedia.org/wiki/Life_imitating_art#cite_note-McGrath1999-1"><i>feel of life</i></a>, through all of it's component parts.
+     What <i>shapes</i> do they take? In my life, they take the shape of the fantastical creativity of video games, the cringy-coolness of anime, the immersive trance of novels or the fantastical imagination of movies and television.
+      <br/>
+      <br/>
+        So now the bad news: still a bit fuzzy on where the closest <b>Chrono-Synclastic Infundibulum</b> actually is... If anyone knows a good inter-galactic literary agent, let's do lunch.
+       <br/>
+       <br/>
+       Oh, And consider donating, I can't imagine postage to Uranus is cheap.
+    </p>
+    <br/>
+    <br/>
   </>
 )
 
