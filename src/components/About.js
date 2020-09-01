ed0cac6f0d25a22a787aedde16f5e095ffec35d3
Seth <sethbalodi@gmail.com>
2020-08-31 22:31:05 -0400

    finished about page, updated

--- src/components/About.js
+++ src/components/About.js
@@ -10,72 +10,91 @@ import Paper from '@material-ui/core/Paper'
  */
 const About = () => (
   <>
-    <h1>What Is All This Here About?</h1>
+
+    <h1>Q: Now What's All This Here About?</h1>
+    <divider/>
+    <h3>A: It's all just a simple, digital <b>Chrono Synclastic Infundibulum</b>.</h3>
 
     <p>
-     This blog is a loose collection of essays, videos and podcasts with no planned coherence.
-     The eventual cosmic fate of these ideas is to become contributions
-     to the <b>Chrono-Synclastic Infundibulae</b>,
-     wherever they may exist throughout our galaxy.
-    </p>
-
-    <p>Some explanation of terms:</p>
-
-    <p>
-      From novelist and scruffy hair enthusiast Kurt Vonnegut's <i>Sirens of Titan</i>, a <b>Chrono-Synclastic Infundibulum</b> is described as the following:
+      From novelist and scruffy hair enthusiast Kurt Vonnegut's <b><i>Sirens of Titan</i></b>:
     </p>
 
     <blockquote>
-      <p>
+      <p style={{ fontSize: '0.95rem' }}>
         <i>
-        "<b><i>Chrono</i></b> means time. 
-         <b><i> Synclastic</i></b> means curved towards the same side in all directions, like the skin of an orange. 
-         <b><i> Infundibulum</i></b> is what ancient Romans like Julius Caesar and Nero called a funnel..."
+        "<b>Chrono</b>" means time. "<b>Synclastic</b>" means curved towards the same side in all directions, like the skin of an orange. "<b>Infundibulum</b>" is what ancient Romans like Julius Caesar and Nero called a funnel...
         </i>
+
+        <br/>
+        <br/>
+
+        <i>
+          These places are where all the different kinds of truths fit togther as nicely as the parts in your solar watch [...] existing as wave phenomena - apparently pulsaing in a distrorted spiral with its origin in the Sun and its terminal in Betelgeuse.
+        </i>"
       </p>
     </blockquote>
 
     <p>
-     Vonnegut describes places in our Universe where all of the myriad lowercase "t" and capital "T" truths co-exist in contradictory harmony to each other, colliding into metaphorical helixes, like philosophical DNA strands. Vonnegut again:
+      Minus the cosmic science fiction, <b>Chrono Synclastic Infundibulums</b> are locations in the universe where <i>subjective truths</i> and <i>capital "T" Truths</i> can coelesce without contradiction. They are an aggregation of all of our stories, which happen to take three fundamental shapes.
     </p>
 
-    <blockquote>
-      <p>
-        <i>
-          "These places are where all the different kinds of truths fit togther as nicely as the parts in your ... solar watch."
-        </i>
-      </p>
-    </blockquote>
+    
+    <p>
+      <ol>
+        <li>The stories we tell about our <b>surroundings</b>; the world.</li>
+        <li>The stories we tell about <b>each other</b>.</li>
+        <li>The stories we tell about <b>ourselves</b>. </li>
+      </ol>
+    </p>
 
     <p>
-      There are nooks and crannies within our own minds where all kinds of truths fit together. We are exceptional at keeping a harmonious cognitive dissonance, which I theorize is part of our strategy to scoff at an uncaring universe, to sneer at the cosmic last laugh.
-      
-      I like the idea that there is a physical place (or places!) where the Universe has decided to do a little consideration of it's own, like a giant cosmic <a href="https://en.wikipedia.org/wiki/The_Thinker">The Thinker</a> sculpture.
-      All good fiction is based on wishful thinking! These <b>Shapes of Stories</b>, as well, represent an honest attempt, goddammit, at wishful thinking.
+      Like shards of broken class, each of these yarns we weave take distinct shapes that fit together to create perception of ourselves. We end up being a <b><i>Wabi Sabi</i></b> pottery bowl of our collective stories on all three levels. Our thoughts, desires and opinions are all the result of the <b>Shapes of our Stories</b>.
     </p>
 
-    <div style={{ margin: 'auto', maxWidth: '300px', width: '100%' }}>
+
+    <div style={{ margin: 'auto', maxWidth: '500px', width: '100%' }}>
       <Paper>
-        <img style={{ marginBottom: '0px' }} src="https://upload.wikimedia.org/wikipedia/commons/b/bf/Le_Penseur_in_the_Jardin_du_Mus%C3%A9e_Rodin%2C_Paris_14_June_2015.jpg"></img>
+        <img style={{ marginBottom: '0px' }} src="https://www.theschooloflife.com/thebookoflife/wp-content/uploads/2016/01/Kintsugi-bowl-honurushi-number-32.jpg"></img>
         <figcaption style={{ marginBottom: '0px' }} >
-          <p style={{ marginBottom: '0px', color: '#928f8f' }}>Fig.1 - He's probably thinking of something wishful.</p>
+          <p style={{ marginBottom: '0px', color: '#928f8f', fontSize: '0.8rem' }}>Fig.1 - Wabi Sabi pottery. The cracks are filled with gold, symbolizing the acceptance of imperfections. In this case, each shard is a story. Together, with a little cosmic glue, we have the sum total of what we think, which is who we are.</p>
         </figcaption>
       </Paper>
     </div>
-    
+    <br/>
+
+
+    <p>
+      This blog is a loose collection <i>essay</i>, <i>videos</i> and <i>podcasts</i> serving as unsophisticated hot takes on <i>movies</i>, <i>video games</i>, <i>novels</i> and <i>philosophy</i> (...well, the fun parts at least). It is an unabashed love letter to all of the sorts of stories we tell.
+    </p>
 
     
-    
     <p>
-    The goal is to have a good heuristic for the <a href="https://en.wikipedia.org/wiki/Life_imitating_art#cite_note-McGrath1999-1"><i>feel of life</i></a>, through all of it's component parts.
-     What <i>shapes</i> do they take? In my life, they take the shape of the fantastical creativity of video games, the cringy-coolness of anime, the immersive trance of novels or the fantastical imagination of movies and television.
-      <br/>
-      <br/>
-        So now the bad news: still a bit fuzzy on where the closest <b>Chrono-Synclastic Infundibulum</b> actually is... If anyone knows a good inter-galactic literary agent, let's do lunch.
-       <br/>
-       <br/>
-       Oh, And consider donating, I can't imagine postage to Uranus is cheap.
+      <h2><b>In whatever capacity I can, I'd like to explore these stories, marvel at their shapes, and maybe even contribute some of my own.</b></h2>
     </p>
+
+
+    {/* <divider/>
+    <p style={{ margin: 'auto' }}>...</p>
+    <divider/>
+    <br/>
+    <div style={{ margin: 'auto', maxWidth: '700px', width: '100%' }}>
+      <Paper>
+        <img style={{ marginBottom: '0px' }} src="https://upload.wikimedia.org/wikipedia/commons/4/4d/Hibernia_locomotive.png"></img>
+        <figcaption style={{ marginBottom: '0px' }} >
+          <p style={{ marginBottom: '0px', color: '#928f8f', fontSize: '0.8rem' }}>Fig.2 - A shape that a story can take. Our stories are the <b>coal</b> being thrown into the cosmic <i>Steam Locomotive</i>.
+
+The <b>Firemen</b> tending to the flames are the artists and scientists and philosophers and directors and novelists that we follow.</p>
+        </figcaption>
+      </Paper>
+    </div>
+    <br/>
+    <br/> */}
+
+    <p>I'm still a bit foggy on the exact location of the closest <b>Chrono-Synclastic Infundibulum</b> to Earth, unfortunately ... If anyone knows a good inter-galactic literary agent, let's do lunch.</p>
+
+    <p>Oh, And consider donating, I can't imagine postage to Uranus is cheap.</p>
+
+
     <br/>
     <br/>
   </>
